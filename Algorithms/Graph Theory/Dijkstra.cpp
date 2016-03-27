5dcc113642d29cbf2ec6cde721c73993a2dcd0e0
Gustavo Ribeiro <gustavofranklin10@gmail.com>
2016-03-26 23:59:15 -0300

    Update Dijkstra.cpp

--- Algorithms/Graph Theory/Dijkstra.cpp
+++ Algorithms/Graph Theory/Dijkstra.cpp
@@ -10,11 +10,11 @@ typedef pair<int, int> edge;
 vector<edge> graph[MAXN];
 int dist[MAXN];
 
-void dijkstra(int n, int s){
-  priority_queue<edge> pq;
+void dijkstra(int s, int n){
+  priority_queue<edge, vector<edge>, greater<edge> > pq;
   edge e;
 
-  for(int i = 0; i < n; i++){
+  for(int i = 1; i <= n; i++){
     dist[i] = INF;
   }
 
@@ -25,9 +25,15 @@ void dijkstra(int n, int s){
     e = pq.top();
     pq.pop();
 
-    if(dist[e.second] < e.first){
+    if(dist[e.second] == e.first){
+      int u = e.second;
       for(int i = 0; i < (int)graph[u].size(); i++){
-
+        int v = graph[u][i].first;
+        int w = graph[u][i].second;
+        if (dist[v] > dist[u] + w) {
+          dist[v] = dist[u] + w;
+          pq.push({dist[v], v})
+        }
       }
     }
   }
