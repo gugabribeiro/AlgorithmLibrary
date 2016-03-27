1e29e9c65ce22b920b974a41acf54fcea080aefc
Gustavo Ribeiro <gustavofranklin10@gmail.com>
2016-03-27 09:36:20 -0300

    Update Floyd-Warshall.cpp

--- Algorithms/Graph Theory/Floyd-Warshall.cpp
+++ Algorithms/Graph Theory/Floyd-Warshall.cpp
@@ -7,9 +7,9 @@ const int N = 301;
 int mat[N][N];
 
 void floydWarshall(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      for (int k = 1; k <= n; k++) {
+  for (int k = 1; k <= n; k++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
         mat[i][j] = min(mat[i][j], mat[i][k] + mat[k][j]);
       }
     }
