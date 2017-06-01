2fa3392a2cc404accf52cbbfb2b4c141749c409a
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2017-06-01 20:45:41 -0300

    Update DSU.cpp

--- Data Structures/DSU/DSU.cpp
+++ Data Structures/DSU/DSU.cpp
@@ -1,46 +1,61 @@
-struct DSU {
-  int n;
-  vector<int> id, sz;
+#include <bits/stdc++.h>
 
-  void init() {
-    for (int i = 0; i < n; i++) {
+using namespace std;
+
+struct DisjointSets {
+  vector<int> id, length;
+
+  DisjointSets(int n) {
+    id.resize(n);
+    length.resize(n);
+    
+    for (int i = 0; i < n; ++i) {
       id[i] = i;
-      sz[i] = 1;
+      length[i] = 1;
     }
   }
-
-  DSU() {}
-  DSU(int n_): 
-    n(n_) {
-      id.resize(n);
-      sz.resize(n);
-      init();
-    }
 
   int find_set(int x) {
     if (id[x] == x) {
       return x;
+    } else {
+      return id[x] = find_set(id[x]);
     }
-    return id[x] = find_set(id[x]);
-  }
-
-  bool same_set(int x, int y) {
-    return (find_set(x) == find_set(y));
   }
 
   bool union_set(int x, int y) {
     x = find_set(x), y = find_set(y);
-    
     if (x != y) {
-      if (sz[x] > sz[y]) {
+      if (length[x] > length[y]) {
         id[y] = x;
-        sz[x] += sz[y];
+        length[x] += length[y];
       } else {
         id[x] = y;
-        sz[y] += sz[x];
+        length[y] += length[x];
       }
       return true;
     }
     return false;
   }
+
+  bool is_same_set(int x, int y) {
+    return find_set(x) == find_set(y);
+  }
 };
+
+int main() {
+  int n, m;
+  scanf("%d %d", &n, &m);
+  DisjointSets banks(n);
+  while (m--) {
+    char c; int u, v;
+    scanf(" %c %d %d", &c, &u, &v);
+    --u, --v;
+    if (c == 'C') {
+      printf("%c\n", banks.is_same_set(u, v) ? 'S' : 'N');
+    } else {
+      banks.union_set(u, v);
+    }
+  }
+  return 0;
+}
