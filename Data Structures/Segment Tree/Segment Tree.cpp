4a98e76544cd71d6b3f13ff97aaddce640b550ea
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-02-14 15:56:35 -0300

    Update Segment Tree.cpp

--- Data Structures/Segment Tree/Segment Tree.cpp
+++ Data Structures/Segment Tree/Segment Tree.cpp
@@ -1,70 +1,62 @@
+template <typename T>
 struct SegmentTree {
-  struct Node {
-    int value; //node attributes
+    int size;
+    vector <T> tree;
 
-    Node() {}
-    Node(int value_): 
-      value(value_) {}
-
-    //merge operator
-    Node operator + (const Node& other) {
-      return Node(value + other.value);
-    }
-  };
-
-  int n;
-  Node neutral = Node(0); // neutral element
-  vector<Node> tree;
-
-  void build(const vector<int>& base, int id, int left, int right) {
-    if (left == right) {
-      tree[id].value = base[left];
-    } else {
-      int mid = (left + right) / 2;
-      build(base, 2 * id + 1, left, mid);
-      build(base, 2 * id + 2, mid + 1, right);
-      tree[id] = tree[2 * id + 1] + tree[2 * id + 2];
-    }
-  }
-
-  SegmentTree() {}
-  SegmentTree(int n_, const vector<int>& base):
-    n(n_) {
-      tree.resize(4 * n);
-      build(base, 0, 0, n - 1);
+    SegmentTree() {}
+    SegmentTree(vector <T> &base) {
+        size = base.size();
+        tree = vector <T> (4 * size);
+        build(0, 0, size - 1, base);
     }
 
-  Node query(int id, int left, int right, int from, int to) {
-    if (right < from or left > to) {
-      return neutral;
-    } else if (left >= from and right <= to) {
-      return tree[id];
-    } else {
-      int mid = (left + right) / 2;
-      return query(2 * id + 1, left, mid, from, to) + 
-             query(2 * id + 2, mid + 1, right, from, to);
+    //Must be implemented
+    T combine(const T &left, const T &right) {
+        return left + right;
     }
-  }
 
-  Node query(int from, int to) {
-    return query(0, 0, n - 1, from, to);
-  }
-
-  void update(int id, int left, int right, int pos, int value) {
-    if (left == right) {
-      tree[id].value = value;
-    } else {
-      int mid = (left + right) / 2;
-      if (pos <= mid) {
-        update(2 * id + 1, left, mid, pos, value);
-      } else {
-        update(2 * id + 2, mid + 1, right, pos, value);
-      }
-      tree[id] = tree[2 * id + 1] + tree[2 * id + 2];
+    void build(int id, int left, int right, vector <T> &base) {
+        if (left == right) {
+            tree[id] = base[left];
+        } else {
+            int mid = (left + right) / 2;
+            build(2 * id + 1, left, mid, base);
+            build(2 * id + 2, mid + 1, right, base);
+            tree[id] = combine(tree[2 * id + 1], tree[2 * id + 2]);
+        }
     }
-  }
 
-  void update(int pos, int value) {
-    update(0, 0, n - 1, pos, value);
-  }
+    void update(int at, T value) {
+        update(0, 0, size - 1, at, value);
+    }
+
+    void update(int id, int left, int right, int at, T value) {
+        if (left == right) {
+            tree[id] = value;
+        } else {
+            int mid = (left + right) / 2;
+            if (at <= mid) {
+                update(2 * id + 1, left, mid, at, value);
+            } else {
+                update(2 * id + 2, mid + 1, right, at, value);
+            }
+            tree[id] = combine(tree[2 * id + 1], tree[2 * id + 2]);
+        }
+    }
+
+    T query(int from, int to) {
+        return query(0, 0, size - 1, from, to);
+    }
+
+    T query(int id, int left, int right, int from, int to) {
+        if (right < from or left > to) {
+            return T();
+        } else if (left >= from and right <= to) {
+            return tree[id];
+        } else {
+            int mid = (left + right) / 2;
+            return combine(query(2 * id + 1, left, mid, from, to),
+                           query(2 * id + 2, mid + 1, right, from, to));
+        }
+    }
 };
