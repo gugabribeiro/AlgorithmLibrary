db5aba8953e740ae2461e28384f1a5290cc66afd
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-02-14 19:59:18 -0300

    Update Persistent Segment Tree.cpp

--- Data Structures/Segment Tree/Persistent Segment Tree.cpp
+++ Data Structures/Segment Tree/Persistent Segment Tree.cpp
@@ -1,78 +1,72 @@
+template <typename T>
 struct PersistentSegmentTree {
-  struct Node {
-    int len, maxOnes, prefOnes, sufOnes;
-    Node* left; Node* right;
+    struct Node {
+        T value;
+        Node *left, *right;
+        Node(T _value = T(), Node *_left = NULL, Node *_right = NULL):
+            value(_value), left(_left), right(_right) {}
+    };
 
-    Node() {}
-    Node(int value):
-      len(1), maxOnes(value), prefOnes(value), sufOnes(value),
-      left(NULL), right(NULL) {}
+    int size;
+    vector <Node *> roots;
 
-    Node operator + (const Node& other) {
-      Node res;
-      res.len = len + other.len;
-      res.maxOnes = max({maxOnes, other.maxOnes, sufOnes + other.prefOnes});
-      res.prefOnes = (prefOnes == len) ? len + other.prefOnes : prefOnes;
-      res.sufOnes = (other.sufOnes == other.len) ? other.len + sufOnes : other.sufOnes;
-      return res;
-    }
-  };
-
-  int n;
-  Node* neutral = new Node(0);
-  vector<Node*> roots;
-
-  Node* build(int left, int right) {
-    Node* newNode = new Node(0);
-    if (left != right) {
-      int mid = (left + right) / 2;
-      newNode->left = build(left, mid);
-      newNode->right = build(mid + 1, right);
-    }
-    return newNode;
-  }
-
-  PersistentSegmentTree() {}
-  PersistentSegmentTree(int n_): 
-    n(n_) {
-      roots.resize(n + 1); //main root + root for every tree
-      roots[0] = build(0, n - 1);
+    PersistentSegmentTree(vector <T> &base) {
+        size = base.size();
+        roots.push_back(build(0, size - 1, base));
     }
 
-  Node query(Node* node, int left, int right, int from, int to) {
-    if (right < from or left > to) {
-      return *neutral;
-    } else if (left >= from and right <= to) {
-      return *node;
-    } else {
-      int mid = (left + right) / 2;
-      return query(node->left, left, mid, from, to) + query(node->right, mid + 1, right, from, to);
+    T combine(const T &left_value, const T &right_value) {
+        return left_value + right_value;
     }
-  }
 
-  Node query(int tree_id, int from, int to) {
-    return query(roots[tree_id], 0, n - 1, from, to);
-  }
-
-  Node* update(Node* node, int left, int right, int pos) {
-    Node* newNode = new Node(1);
-    if (left != right) {
-      int mid = (left + right) / 2;
-      if (pos <= mid) {
-        newNode->left = update(node->left, left, mid, pos);
-        newNode->right = node->right;
-      } else {
-        newNode->left = node->left;
-        newNode->right = update(node->right, mid + 1, right, pos);
-      }
-      Node aux = (*newNode->left) + (*newNode->right);
-      newNode->len = aux.len; newNode->maxOnes = aux.maxOnes;
-      newNode->prefOnes = aux.prefOnes; newNode->sufOnes = aux.sufOnes;
+    Node *build(int left, int right, vector <T> &base) {
+        Node *new_node = new Node();
+        if (left == right) {
+            new_node->value = base[left];
+        } else {
+            int mid = (left + right) / 2;
+            new_node->left  = build(left, mid, base);
+            new_node->right = build(mid + 1, right, base);
+            new_node->value = combine(new_node->left->value, new_node->right->value);
+        }
+        return new_node;
     }
-    return newNode;
-  }
 
-  void update(int tree_id, int pos) {
-    roots[tree_id] = update(roots[tree_id - 1], 0, n - 1, pos);
-  }
+    T query(int root_id, int from, int to) {
+        return query(roots[root_id], 0, size - 1, from, to);
+    }
+
+    T query(Node *node, int left, int right, int from, int to) {
+        if (right < from or left > to) {
+            return T();
+        } else if (left >= from and right <= to) {
+            return node->value;
+        } else {
+            int mid = (left + right) / 2;
+            return combine(query(node->left, left, mid, from, to),
+                           query(node->right, mid + 1, right, from, to));
+        }
+    }
+
+    void update(int at, const T &value) {
+        roots.push_back(update(roots.back(), 0, size - 1, at, value));
+    }
+
+    Node *update(Node *node, int left, int right, int at, const T &value) {
+        Node *new_node = new Node();
+        if (left == right) {
+            new_node->value = value;
+        } else {
+            int mid = (left + right) / 2;
+            if (at <= mid) {
+                new_node->left  = update(node->left, left, mid, at, value);
+                new_node->right = node->right;
+            } else {
+                new_node->left  = node->left;
+                new_node->right = update(node->right, mid + 1, right, at, value);
+            }
+            new_node->value = combine(new_node->left->value, new_node->right->value);
+        }
+        return new_node;
+    }
 };
