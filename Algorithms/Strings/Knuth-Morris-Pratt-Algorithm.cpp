1b0e98c9fdcd935f338a048f1f8472c8dc5b9ef7
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-03-25 23:42:52 -0300

    Update Knuth-Morris-Pratt-Algorithm.cpp

--- Algorithms/Strings/Knuth-Morris-Pratt-Algorithm.cpp
+++ Algorithms/Strings/Knuth-Morris-Pratt-Algorithm.cpp
@@ -1,29 +1,29 @@
-vector<int> prefix_function(const string &s) {
-  int n = s.size();
-  vector<int> back(n + 1); back[0] = -1;
-  int i = 0, j = -1;
-  while (i < n) {
-    while (j >= 0 and s[i] != s[j]) {
-      j = back[j];
+struct KMP {
+    static vector <int> prefix_function(const vector <int> &T) {
+        int i = 0, j, n = T.size();
+        vector <int> back(n + 1);
+        back[0] = j = -1;
+        while (i < n) {
+            while (j >= 0 and T[i] != T[j]) { j = back[j]; }
+            ++i;
+            ++j;
+            back[i] = j;
+        }
+        return back;
     }
-    ++i, ++j;
-    back[i] = j;
-  }
-  return back;
-}
 
-vector<int> kmp(const string &s, const string &t) {
-  int i = 0, j = 0, n = s.size(), m = t.size();
-  vector<int> back = prefix_function(t), occur;
-  while (i < n) {
-    while (j >= 0 and s[i] != t[j]) {
-      j = back[j];
+    static vector <int> matching(const vector <int> &S, const vector <int> &T) {
+        int i = 0, j = 0, n = S.size(), m = T.size();
+        vector <int> back = prefix_function(T), at;
+        while (i < n) {
+            while (j >= 0 and S[i] != T[j]) { j = back[j]; }
+            ++i;
+            ++j;
+            if (j == m) {
+                at.push_back(i - j);
+                j = back[j];
+            }
+        }
+        return at;
     }
-    ++i, ++j;
-    if (j == m) {
-      occur.push_back(i - j);
-      j = back[j];
-    }
-  }
-  return occur;
-}
+};
