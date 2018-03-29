611b47d3885384af0ee8c7ebf8a305fda5cdc6df
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-03-29 12:09:21 -0300

    Cache optimization

--- Data Structures/Sparse Table/Sparse Table.cpp
+++ Data Structures/Sparse Table/Sparse Table.cpp
@@ -1,31 +1,32 @@
 template <typename T>
 struct sparse_table {
-    #define LOG(len) ((int)floor(log((double)len) / log(2.0)))
-
-    int n, m;
+    int n;
+    vector <int> logs;
     vector <vector <T> > table;
 
-    //Must be implemented according to the problem
     T merge(const T &a, const T &b) {
-        return __gcd(a, b);
+        return max(a, b);
     }
 
     sparse_table(const vector <T> &base) {
         n = base.size();
-        m = LOG(n) + 1;
-        table = vector <vector <T> > (n, vector <T> (m));
+        logs = vector <int> (n + 1);
+        for (int i = 2; i <= n; ++i) {
+            logs[i] = logs[i / 2] + 1;
+        }
+        table = vector <vector <T> > (logs[n] + 1, vector <T> (n));
         for (int i = 0; i < n; ++i) {
-            table[i][0] = base[i];
+            table[0][i] = base[i];
         }
         for (int j = 1; (1 << j) <= n; ++j) {
             for (int i = 0; i + (1 << j) - 1 < n; ++i) {
-                table[i][j] = merge(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+                table[j][i] = merge(table[j - 1][i], table[j - 1][i + (1 << (j - 1))]);
             }
         }
     }
 
     T query(int l, int r) {
-        int k = LOG(r - l + 1);
-        return merge(table[l][k], table[r - (1 << k) + 1][k]);
+        int k = logs[r - l + 1];
+        return merge(table[k][l], table[k][r - (1 << k) + 1]);
     }
 };
